433b553b37ceaffb1e37b76e21f72d3740499588
Guillaume <glouvel@biologie.ens.fr>
2019-01-15 16:25:04 +0100

    Print the age of each taxon

---
+++ treeTools/getAges.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from __future__ import print_function
+from LibsDyogen import myTools, myPhylTree
+
+
+if __name__ == '__main__':
+    args = myTools.checkArgs([('phyltree', myTools.File)], [], __doc__)
+    phyltree = myPhylTree.PhylogeneticTree(args['phyltree'])
+
+    for taxon, age in sorted(phyltree.ages.items(), key=lambda x: (x[1], x[0])):
+        print(taxon + '\t' + '%7g' % age)
